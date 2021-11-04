613119398589243fdf13ce0d810524db247474ac
HerbiShtini <herb_sh@hotmail.com>
2021-11-05 00:06:46 +0100

    Migration color and average value line

--- plots/migration.R
+++ plots/migration.R
@@ -1,12 +1,13 @@
 if (!require("plotly")) install.packages("plotly"); library(plotly)
+if (!require("zoo")) install.packages('zoo'); library(zoo)
 
 ### Import packages
 library(rio)
 library(tidyverse)
+library(zoo)
 #
 source("utilities.R")
 
-options( scipen = 999 )
 
 migration <- import("data/migration.csv", 
                     encoding = "UTF-8", 
@@ -21,16 +22,16 @@ netMigrationTotalFormat <- setFertilityRateHeaders(netMigrationTotal)
 netMigrationTotalFormat$Insgesamt <- as.numeric(as.character(netMigrationTotalFormat$Insgesamt))
 
 netMigrationTotalFormat <- netMigrationTotalFormat %>% 
-  mutate(color = ifelse(Insgesamt>0, "#3AC0DA", "#eea29a"))
+  mutate(fiveYearAverage = (rollmeanr(Insgesamt, k=3, fill=Insgesamt)),
+         color = ifelse(Insgesamt>0, "#3AC0DA", "#eea29a"))
 
 #Plot
 plotMigration <- function () {
   plot <- ggplot(netMigrationTotalFormat, 
                  aes(x=years, y=Insgesamt,
-                     text=paste("Jahr: ", years, " Bevölkerungszahl: ", Insgesamt))) +
-   # geom_line(aes(x=years, y=Insgesamt, group = 1), stat="identity") +
+                     text=paste("Jahr: ", years, "<br>Bevölkerungszahl: ", formatC(Insgesamt, format="f", big.mark=",", digits=0)))) +
+    geom_line(aes(x=years, y=fiveYearAverage, group = 1), stat="identity") +
     geom_segment( aes(x=years, xend=years, y=0, yend=Insgesamt, color = color), size=4, alpha=0.9) +
-    #geom_segment( aes(x=years, xend=years, y=0, yend=weiblich, color = color), size=1, alpha=0.9) +
     scale_colour_identity() +
     theme_light() +
     labs(y="Bevölkerungszahl",
@@ -40,7 +41,10 @@ plotMigration <- function () {
       legend.position = "none",
       panel.border = element_blank(),
       axis.text.x = element_text(angle=65, vjust=0.6)
-    )
+    ) +
+    scale_y_continuous(labels = function(n) {
+      formatC(n, format="f", big.mark=",", digits=0)
+    })
   
   return(ggplotly(plot, tooltip = c("text")))
 }
